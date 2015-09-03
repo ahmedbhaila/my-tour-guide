932d2df3670dcb227d1539204cb169f58d32d551
Ahmed Bhaila <ahmedajmal.bhaila@hyatt.com>
2015-09-02 23:59:30 -0500

    Sending text alert on confirmation

--- src/main/java/com/mycompany/WhispirMessageReceiver.java
+++ src/main/java/com/mycompany/WhispirMessageReceiver.java
@@ -2,10 +2,17 @@ package com.mycompany;
 
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 
 public class WhispirMessageReceiver {
 	@Autowired
 	RabbitTemplate rabbitTemplate;
+	
+	@Autowired
+	WhispirService whispirService;
+	
+	@Value("${whispir.message.template.confirm.alert}")
+	String alertTemplate;
 	
 	public void handleWhispirMessage(WhispirCallbackMessage message) {
 		String content = message.getResponseMessage().getContent();
@@ -14,6 +21,12 @@ public class WhispirMessageReceiver {
 		
 		if(content.equals("1")) {
 			// user wants to register to receive alerts
+			
+			// save this preference in datastore
+			
+			// send another text message to this user indicating their choice
+			whispirService.sendSMS(message.getSource().getVoice(), alertTemplate, "You have chosen to receive alerts from your location");
+			
 		}
 		
 		else if(content.equalsIgnoreCase("stop")) {
