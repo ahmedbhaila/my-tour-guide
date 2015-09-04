372996bc11b08d52c78cba7a62dab6a0e5981d19
Ahmed Bhaila <ahmedajmal.bhaila@hyatt.com>
2015-09-03 20:31:11 -0500

    Support for stopping/subscribing to alerts

--- src/main/java/com/mycompany/TourGuideMessageService.java
+++ src/main/java/com/mycompany/TourGuideMessageService.java
@@ -2,6 +2,7 @@ package com.mycompany;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.data.redis.core.RedisTemplate;
 
 public class TourGuideMessageService {
 	
@@ -16,7 +17,17 @@ public class TourGuideMessageService {
 	@Autowired
 	WhispirService whispirService;
 	
+	@Autowired
+	RedisTemplate<String, String> redisTemplate;
+	
 	public void sendWelcomeMessage(String phone, String name) {
 		whispirService.sendSMS(phone, messageTemplateId, "TourGuideCallback test", messageBody.replace(NAME, name));
+	}
+	
+	public void sendGenericMessage(String phone, String message) {
+		String alert = (String) redisTemplate.opsForHash().get("subscriber:" + phone, "alerts");
+		if(alert.equals("true")) {
+			whispirService.sendSMS(phone, messageTemplateId, null, message);
+		}
 	}
 }
--- src/main/java/com/mycompany/WhispirMessageReceiver.java
+++ src/main/java/com/mycompany/WhispirMessageReceiver.java
@@ -3,6 +3,7 @@ package com.mycompany;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.data.redis.core.RedisTemplate;
 
 public class WhispirMessageReceiver {
 	@Autowired
@@ -14,23 +15,30 @@ public class WhispirMessageReceiver {
 	@Value("${whispir.message.template.confirm.alert}")
 	String alertTemplate;
 	
+	@Autowired
+	RedisTemplate<String, String> redisTemplate;
+	
+	
 	public void handleWhispirMessage(WhispirCallbackMessage message) {
 		String content = message.getResponseMessage().getContent();
 		
-		System.out.println("Hello, I received content " + content);
-		
-		if(content.equals("1")) {
+		if(content.equalsIgnoreCase("sub")) {
 			// user wants to register to receive alerts
 			
 			// save this preference in datastore
+			redisTemplate.opsForHash().put("subscriber:" + message.getSource().getVoice(), "alerts", "true");
 			
 			// send another text message to this user indicating their choice
-			whispirService.sendSMS(message.getSource().getVoice(), alertTemplate, null, "You have chosen to receive alerts from your location");
+			whispirService.sendSMS(message.getSource().getVoice(), alertTemplate, null, "You have chosen to receive alerts from your location, if you ever want to stop receiving alert just type STOP and send to this number");
 			
 		}
 		
 		else if(content.equalsIgnoreCase("stop")) {
 			// user wants to stop receiving alerts
+			whispirService.sendSMS(message.getSource().getVoice(), alertTemplate, null, "We have received your request to not send alerts anymore, if you would like to receive alerts again please type SUB and send to this number");
+			
+			// save this preference in datastore
+			redisTemplate.opsForHash().put("subscriber:" + message.getSource().getVoice(), "alerts", "false");
 		}
 		// and so son
 		
